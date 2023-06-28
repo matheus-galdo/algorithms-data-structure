e66d2645d6ea608d0ce8a86215ecd653558bd380
Matheus Galdino <matheus.kho@gmail.com>
2023-06-28 09:06:00 -0300

    refactor: renaming recursivaSum file and function

--- basicAlgorithms/recursivaSum.js
+++ basicAlgorithms/recursivaSum.js
@@ -1,17 +1,17 @@
 
-function somaRecursiva(lista) {
+function recursivaSum(lista) {
     if (lista.length === 1) {
         console.log('caso base', lista);
         return lista[0];
     }else{
         let listaNova = [...lista];
         listaNova.splice(0,1);
-        const result = lista[0] + somaRecursiva(listaNova);
+        const result = lista[0] + recursivaSum(listaNova);
         console.log('o resultado da lista atual é:', result, [...lista]);
         return result;
     }
 }
 
-const r = somaRecursiva([2,4,6]);
+const r = recursivaSum([2,4,6]);
 
 console.log(r);
